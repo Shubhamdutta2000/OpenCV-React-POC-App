3246b7484fdb0940db037b5f4dc43a8af4c7b3cc
Shubhamdutta2000 <dondutta2000@gmail.com>
2023-08-13 02:40:53 +0530

    added files for open cv poject to render video camera with fastapi and render the video camea as jpg image in react app

---
+++ open_cv_fastapi/main.py
@@ -0,0 +1,34 @@
+from fastapi import FastAPI, Request, Response, BackgroundTasks
+from fastapi.responses import StreamingResponse
+import cv2
+import numpy as np
+
+app = FastAPI()
+
+class VideoCamera(object):
+    def __init__(self):
+        self.video = cv2.VideoCapture(0)
+    def __del__(self):
+        self.video.release()
+    def get_frame(self):
+        ret, frame = self.video.read()
+        ret, jpeg = cv2.imencode('.jpg', frame)
+        return jpeg.tobytes()
+
+def gen(camera):
+    while True:
+        frame = camera.get_frame()
+        yield (b'--frame\r\n'
+               b'Content-Type: image/jpeg\r\n\r\n' + frame + b'\r\n\r\n')
+
+@app.get("/")
+def read_root(request: Request):
+    return {
+        "message": "Hello User 🙏"
+    }
+
+@app.get("/video_feed")
+async def video_feed():
+    return StreamingResponse(gen(VideoCamera()),
+                             media_type='multipart/x-mixed-replace; boundary=frame')
+
---
+++ react_app/src/App.js
@@ -0,0 +1,13 @@
+import React from "react";
+
+const Cam = () => {
+return (
+  <div>
+   <img
+    src="http://localhost:8000/video_feed"
+    alt="Video"
+   />
+  </div>
+ );
+};
+export default Cam;
