60c6e53187f4f64c81be43766dee22a67e0ebdb0
Anindya Dey <105655927+AnindyaDEV@users.noreply.github.com>
2023-08-15 00:53:39 +0530

    backend integration stage 1 Anindya

--- open_cv_fastapi/main.py
+++ open_cv_fastapi/main.py
@@ -1,9 +1,22 @@
-from fastapi import FastAPI, Request, Response, BackgroundTasks
+from fastapi import FastAPI, Request, Response, BackgroundTasks,HTTPException
 from fastapi.responses import StreamingResponse
 from camera import VideoCamera
+from fastapi import FastAPI
+from fastapi.middleware.cors import CORSMiddleware
 
 app = FastAPI()
 
+origins = ["*"]
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=origins,
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
+global video_resource
+video_resource = VideoCamera()
 # class SimpleVideoCamera(object):
 #     def __init__(self):
 #         self.video = cv2.VideoCapture(0)
@@ -28,6 +41,16 @@ def read_root(request: Request):
 
 @app.get("/video_feed")
 async def video_feed():
-    return StreamingResponse(gen(VideoCamera()),
+    video_resource = VideoCamera()
+    return StreamingResponse(gen(video_resource),
                              media_type='multipart/x-mixed-replace; boundary=frame')
 
+
+@app.delete("/stop")
+async def stop_video():
+    
+    try:
+        video_resource.__del__()
+        return {"message": "Video resource released"}
+    except Exception as e:
+        raise HTTPException(status_code=500, detail=str(e))
